e8469a0d754dd3be680370ad49b8dcc3e171a58c
Charles Blake <cb@cblake.net>
2016-02-01 08:07:54 -0500

    Remove defunct tests.

--- test/noDefault1fail.nim
+++
@@ -1,8 +0,0 @@
-proc demo(alpha=1, verb=false, flag: bool): int =
-  ## demo entry point with varied, meaningless parameters.
-  echo "alpha:", alpha, " verb:", verb, " flag:", repr(flag)
-  return 42
-
-when isMainModule:
-  import cligen
-  dispatch(demo)
--- test/noDefault2fail.nim
+++
@@ -1,8 +0,0 @@
-proc demo(alpha=1, verb: bool, files: seq[string]): int =
-  ## demo entry point with varied, meaningless parameters.
-  echo "alpha:", alpha, " verb:", verb, " files:", repr(files)
-  return 42
-
-when isMainModule:
-  import cligen
-  dispatch(demo)
