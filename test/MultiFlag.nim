a6f34f1b45cf8457796c4b79dbaf33525f3fef7c
Charles Blake <cb@cblake.net>
2016-02-06 12:14:58 -0500

    Multiple bools test prog for -avb4 type short opts.

---
+++ test/MultiFlag.nim
@@ -0,0 +1,9 @@
+proc demo(alpha=false, beta=2, verb=false, args: seq[string]): int =
+  ## demo entry point with varied, meaningless parameters.
+  echo "alpha:", alpha, " beta:", beta, " verb:", verb
+  for i, arg in args: echo "positional[", i, "]: ", repr(arg)
+  return 42
+
+when isMainModule:
+  import cligen
+  dispatch(demo, stopWords = @[ "hi", "ho" ])
