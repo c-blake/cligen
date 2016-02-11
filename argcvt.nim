7f3dc6757ec3f4256845ed3a86d531f33314b8ad
Charles Blake <cb@cblake.net>
2016-02-11 10:07:28 -0500

    Round out support for basic types. (Should add range/sign checking input validations)

--- argcvt.nim
+++ argcvt.nim
@@ -39,7 +39,6 @@ proc alignTable*(tab: seq[array[0..3, string]],
 ## argParse determines how string arguments are interpreted into native types
 ## while argHelp explains this interpretation to a command-line user.
 
-## XXX add many more pairs for, e.g. all varieties of int, float.
 # bool
 template argParse*(dst: bool, key: string, val: string, help: string) =
   if len(val) > 0:
@@ -65,6 +64,16 @@ template argHelp*(helpT: seq[array[0..3, string]], defVal: string,
                   parNm: string, sh: string, parHelp: string) =
   helpT.add([ keys(parNm, sh), "string", "\"" & defVal & "\"", parHelp ])
 
+# cstring
+template argParse*(dst: cstring, key: string, val: string, help: string) =
+  if val == nil:
+    argRet(1, "Bad value nil for string param \"$1\"\n$2" % [ key, help ])
+  dst = val
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: cstring,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "string", "\"" & $defVal & "\"", parHelp ])
+
 # char
 template argParse*(dst: char, key: string, val: string, help: string) =
   if val == nil or len(val) > 1:
@@ -86,6 +95,114 @@ template argHelp*(helpT: seq[array[0..3, string]], defVal: int,
                   parNm: string, sh: string, parHelp: string) =
   helpT.add([ keys(parNm, sh), "int", $defVal, parHelp ])
 
+# int8
+template argParse*(dst: int8, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting int8\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = int8(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: int8,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "int8", $defVal, parHelp ])
+
+# int16
+template argParse*(dst: int16, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting int16\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = int16(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: int16,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "int16", $defVal, parHelp ])
+
+# int32
+template argParse*(dst: int32, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting int32\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = int32(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: int32,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "int32", $defVal, parHelp ])
+
+# int64
+template argParse*(dst: int64, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting int64\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = tmp
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: int64,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "int64", $defVal, parHelp ])
+
+# uint
+template argParse*(dst: uint, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting uint\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = uint(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: uint,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "uint", $defVal, parHelp ])
+
+# uint8
+template argParse*(dst: uint8, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting uint8\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = uint8(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: uint8,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "uint8", $defVal, parHelp ])
+
+# uint16
+template argParse*(dst: uint16, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting uint16\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = uint16(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: uint16,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "uint16", $defVal, parHelp ])
+
+# uint32
+template argParse*(dst: uint32, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting uint32\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = uint32(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: uint32,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "uint32", $defVal, parHelp ])
+
+# uint64
+template argParse*(dst: uint64, key: string, val: string, help: string) =
+  var tmp: int
+  if val == nil or parseInt(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting uint64\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = uint64(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: uint64,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "uint64", $defVal, parHelp ])
+
 # float
 template argParse*(dst: float, key: string, val: string, help: string) =
   if val == nil or parseFloat(strip(val), dst) == 0:
@@ -95,3 +212,25 @@ template argParse*(dst: float, key: string, val: string, help: string) =
 template argHelp*(helpT: seq[array[0..3, string]], defVal: float,
                   parNm: string, sh: string, parHelp: string) =
   helpT.add([ keys(parNm, sh), "float", $defVal, parHelp ])
+
+# float32
+template argParse*(dst: float32, key: string, val: string, help: string) =
+  var tmp: float
+  if val == nil or parseFloat(strip(val), tmp) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting float32\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+  else: dst = float32(tmp)
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: float32,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "float32", $defVal, parHelp ])
+
+# float64
+template argParse*(dst: float64, key: string, val: string, help: string) =
+  if val == nil or parseFloat(strip(val), dst) == 0:
+    argRet(1, "Bad value: \"$1\" for option \"$2\"; expecting float64\n$3" %
+           [ (if val == nil: "nil" else: val), key, help ])
+
+template argHelp*(helpT: seq[array[0..3, string]], defVal: float64,
+                  parNm: string, sh: string, parHelp: string) =
+  helpT.add([ keys(parNm, sh), "float64", $defVal, parHelp ])
---
+++ test/AllTypes.nim
@@ -0,0 +1,40 @@
+proc demo(aa: bool = false,
+          bb: string = "hi",
+          cc: cstring = "ho",
+          dd: char = 'X',
+          ee: int = 1,
+          ff: int8 = 2,
+          gg: int16 = 3,
+          hh: int32 = 4,
+          ii: int64 = 5,
+          jj: uint = 6,
+          kk: uint8 = 7,
+          ll: uint16 = 8,
+          mm: uint32 = 9,
+          nn: uint64 = 10,
+          oo: float = 11,
+          pp: float32 = 12,
+          qq: float64 = 13,
+          args: seq[string]): int =
+  ## demo entry point with parameters of all basic types.
+  echo "aa: ", aa
+  echo "bb: ", bb
+  echo "cc: ", cc
+  echo "dd: ", dd
+  echo "ee: ", ee
+  echo "ff: ", ff
+  echo "gg: ", gg
+  echo "hh: ", hh
+  echo "ii: ", ii
+  echo "jj: ", jj
+  echo "kk: ", kk
+  echo "ll: ", ll
+  echo "mm: ", mm
+  echo "nn: ", nn
+  echo "oo: ", oo
+  echo "pp: ", pp
+  echo "qq: ", qq
+  for i, arg in args: echo "positional[", i, "]: ", repr(arg)
+  return 42
+
+when isMainModule: import cligen; dispatch(demo)
