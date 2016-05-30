6baa82d84227b063e770f46655f6d5f0764e8d91
Charles Blake <cb@cblake.net>
2016-05-30 11:27:32 -0400

    Propagate script args to cmds; Ignore .ndb files.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for n in test/[A-Z]*; do
   echo "Test: $n"
-  nim c --run $n --help 2>&1 | grep -v '^CC:'
+  nim c "$@" --run $n --help 2>&1 | grep -v '^CC:'
   echo "===================================="
 done
