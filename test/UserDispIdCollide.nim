f465ff4bcd7ecb379d197a11712ae161486cbdb9
Charles Blake <cb@cblake.net>
2016-09-28 07:48:50 -0400

    Use purpose of program for test command doc.

--- test/UserDispIdCollide.nim
+++ test/UserDispIdCollide.nim
@@ -1,8 +1,6 @@
-# This tests if things work if the wrapped user-proc uses identifiers also in
-# our generated dispatch proc.  Should be fine as long as no imports pull in a
-# name like "disapatcherFoo".
-
 proc demo(usage=1, cmdline="bad name", getopt="ho", args: seq[string]): int =
+  ## This tests if things work when a wrapped user-proc uses identifiers also
+  ## used in our generated dispatch proc.
   echo "usage:", usage, " cmdline:", repr(cmdline), " getopt:", getopt
   for i, arg in args: echo "positional[", i, "]: ", repr(arg)
   return 42
