a8a7821cb63d3bc6832751066d7c37cb75d1776e
Charles Blake <cb@cblake.net>
2017-11-22 01:59:01 -0500

    Use toNimIdent not now deprecated `!`.

--- cligen.nim
+++ cligen.nim
@@ -158,7 +158,7 @@ macro dispatchGen*(pro: typed, cmdName: string="", doc: string="",
     cmtDoc = strip(cmtDoc)
   let proNm = $pro                      # Name of wrappred proc
   let cName = if len($cmdName) == 0: proNm else: $cmdName
-  let disNm = !("dispatch" & $pro)      # Name of dispatch wrapper
+  let disNm = toNimIdent("dispatch" & $pro) # Name of dispatch wrapper
   let posIx = posIxGet(positional, fpars) #param slot for positional cmd args|-1
   let shOpt = dupBlock(fpars, posIx, parseShorts(short))
   var spars = copyNimTree(fpars)        # Create shadow/safe prefixed params.
