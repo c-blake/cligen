bcb5df3c48168b01f14ec3ba898c6193ee5ae153
Charles Blake <cb@cblake.net>
2018-04-18 13:27:09 -0400

    Oops - forgot to git add the new test program.

---
+++ test/ImplicitDefault.nim
@@ -0,0 +1,14 @@
+var foo=2.0
+
+proc demo(alpha: int, beta=foo, verb=false, item: string, args: seq[string]): int =
+  ## demo entry point with varied, meaningless parameters.  A Nim invocation
+  ## might be: demo(alpha=2, @[ "hi", "ho" ]) corresponding to the command
+  ## invocation "demo --alpha=2 hi ho" (assuming executable gets named demo).
+  echo "alpha:", alpha, " beta:", beta, " verb:", verb, " item:", repr(item)
+  for i, arg in args: echo "positional[", i, "]: ", repr(arg)
+  return 42
+
+when isMainModule:
+  import cligen, argcvt
+  argcvt.REQUIRED = "MANDATORY"
+  dispatch(demo, implicitDefault = @[ "item" ])
