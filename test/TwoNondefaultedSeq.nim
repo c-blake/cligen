1f6e4df71a34b2d2ddb59beab49dbd4a492d9665
Charles Blake <cb@cblake.net>
2018-04-26 07:34:53 -0400

    Add Delimiter-Prefixed Separated Value argParse/Help pair for seq[string]. Should generalize this to seq[T] or at least also seq[numbers]. Use/test the new parser in ``test/TwoNondefaultedSeq*``.

--- test/TwoNondefaultedSeq.nim
+++ test/TwoNondefaultedSeq.nim
@@ -5,18 +5,5 @@ proc demo(alpha=1, verb=false, args: seq[string], stuff: seq[string]): int=
   return 42
 
 when isMainModule:
-  from strutils import split, `%`, join
-  from argcvt   import keys, argRet, argRq  # Little helpers
-
-  template argParse(dst: seq[string], key: string, dfl: seq[string], val: string, help: string) =
-    if val == nil:
-      argRet(1, "Bad value nil for CSV param \"$1\"\n$2" % [ key, help ])
-    dst = val.split(",")
-
-  template argHelp(ht: seq[seq[string]], defVal: seq[string],
-                   parNm: string, sh: string, parHelp: string, rq: int) =
-    ht.add(@[ keys(parNm, sh), "CSV",
-              argRq(rq, "\"" & defVal.join(",") & "\""), parHelp])
-
-  import cligen
+  import cligen, argSeqDPSV
   dispatch(demo)
--- test/TwoNondefaultedSeqSwap.nim
+++ test/TwoNondefaultedSeqSwap.nim
@@ -1,22 +1,9 @@
 proc demo(alpha=1, verb=false, args: seq[string], stuff: seq[string]): int=
   ## demo entry point with varied, meaningless parameters.
   echo "alpha:", alpha, " verb:", verb, " stuff:", repr(stuff)
-  for i, arg in args: echo "positional[", i, "]: ", repr(arg)
+  for i, arg in args: echo "args[", i, "]: ", repr(arg)
   return 42
 
 when isMainModule:
-  from strutils import split, `%`, join
-  from argcvt   import keys, argRet, argRq  # Little helpers
-
-  template argParse(dst: seq[string], key: string, dfl: seq[string], val: string, help: string) =
-    if val == nil:
-      argRet(1, "Bad value nil for CSV param \"$1\"\n$2" % [ key, help ])
-    dst = val.split(",")
-
-  template argHelp(ht: seq[seq[string]], defVal: seq[string],
-                   parNm: string, sh: string, parHelp: string, rq: int) =
-    ht.add(@[ keys(parNm, sh), "CSV",
-              argRq(rq, "\"" & defVal.join(",") & "\""), parHelp])
-
-  import cligen
+  import cligen, argSeqDPSV
   dispatch(demo, positional="stuff")
