58f83f2929382f7a2e29b6537b4ddf32cbcb5137
Charles Blake <cb@cblake.net>
2018-04-28 03:37:30 -0400

    Though seq[generic enum] is currently not working, importing argcvt and manually instantiating is not so hard.  So, have test/Enums.nim show this.

--- test/Enums.nim
+++ test/Enums.nim
@@ -1,14 +1,14 @@
 type Color = enum red, green, blue
 
-#proc demo(background=red, foregrounds = @[green], args: seq[string]) =
-proc demo(background=red, foregrounds=green, args: seq[string]) =
+proc demo(background=red, foregrounds = @[green], args: seq[string]) =
   ## demo entry point with varied, meaningless parameters.
   echo "background:", background, " foregrounds:", foregrounds
   for i, arg in args: echo "positional[", i, "]: ", repr(arg)
 
 when isMainModule:
-  import cligen
-# let seqDelimit = ","
+  import cligen, argcvt
+  let seqDelimit = ","
+  argParseHelpSeq(Color)
   dispatch(demo,
            help = { "background"  : "background color",
                     "foregrounds" : "foreround colors" })
