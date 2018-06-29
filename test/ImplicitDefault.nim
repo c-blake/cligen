5c1931a4637dd05906e5e0fccfc1f909979d931e
Charles Blake <cb@cblake.net>
2018-06-29 04:41:54 -0400

    Rename ``version`` to ``bypass`` so that people don't get the wrong idea how to proceed with --version-type activity.

--- test/ImplicitDefault.nim
+++ test/ImplicitDefault.nim
@@ -1,15 +1,15 @@
-proc demo(alpha: int, version=false, item: string, args: seq[string]): int =
+proc demo(alpha: int, bypass=false, item: string, args: seq[string]): int =
   ## demo entry point with varied, meaningless parameters.  A Nim invocation
   ## might be: demo(alpha=2, @[ "hi", "ho" ]) corresponding to the command
   ## invocation "demo --alpha=2 hi ho" (assuming executable gets named demo).
-  if version:
+  if bypass:
     echo "0.0.0"
     return 0
-  echo "alpha:", alpha, " version:", version, " item:", repr(item)
+  echo "alpha:", alpha, " bypass:", bypass, " item:", repr(item)
   for i, arg in args: echo "positional[", i, "]: ", repr(arg)
   return 42
 
 when isMainModule:
   import cligen
   dispatch(demo,
-           implicitDefault = @[ "item" ], mandatoryOverride = @[ "version" ])
+           implicitDefault = @[ "item" ], mandatoryOverride = @[ "bypass" ])
