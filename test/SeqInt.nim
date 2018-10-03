8e633e50b9e30774e0ee9724ae59d78804abe567
Charles Blake <cb@cblake.net>
2018-10-03 15:17:47 -0400

    Fix incorrect doc-comment.

--- test/SeqInt.nim
+++ test/SeqInt.nim
@@ -1,7 +1,7 @@
 proc demo(args: seq[int], alpha=1, beta='\l', verb=false, item=""): int =
   ## demo entry point with varied, meaningless parameters.  A Nim invocation
   ## might be: demo(alpha=2, @[ "hi", "ho" ]) corresponding to the command
-  ## invocation "demo --alpha=2 hi ho" (assuming executable gets named demo).
+  ## invocation "demo --alpha=2 3 4" (assuming executable gets named demo).
   echo "alpha:", alpha, " beta:", beta, " verb:", verb, " item:", repr(item)
   for i, arg in args: echo "positional[", i, "]: ", repr(arg)
   return 42
