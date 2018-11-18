bda131770cd7674a988cf8b362162bbf71e68063
Charles Blake <cb@cblake.net>
2018-11-18 08:55:36 -0500

    Use wider C type since also used as index into (n+2)*(m+2) which gets big fast. Also, strengthen length diff check bound as much as possible (but no more).

--- cligen/textUt.nim
+++ cligen/textUt.nim
@@ -36,15 +36,16 @@ proc alignTable*(tab: TextTab, prefixLen=0, colGap=2, minLast=16, rowSep="",
       result &= repeat(" ", leader) & wrapped[j] & "\n"
     result &= rowSep
 
-type C = uint8      ##Type for edit cost values & totals
-proc distDamerau[T](A, B: openArray[T], maxDist=uint8.high,
-                    Cid=1.C, Csub=1.C, Cxpo=1.C, dI: var seq[C]): C =
+type C = int16      ##Type for edit cost values & totals
+type Cmx = C.high
+proc distDamerau*[T](A, B: openArray[T], maxDist=Cmx,
+                     Cid=1.C, Csub=1.C, Cxpo=1.C, dI: var seq[C]): C =
   ## True Damerau(1964) distance with unrestricted transpositions.
   var n = A.len                         #ensure 2nd arg shorter (m < n)
   var m = B.len     #XXX Ukkonen/Berghel or even faster Myers/Hyyro?
   if n < m:         #XXX Unlikely to matter for juat a few short strings.
     return distDamerau(B, A, maxDist, Cid, Csub, Cxpo, dI)
-  if n - m > int(maxDist) * int(Cid) + 1:  #XXX Check strictness of this bound
+  if n - m >= int(maxDist) * int(Cid):
     return maxDist
   let CsubA = min(2.C * Cid, Csub)      #Can always do a sub w/del + ins
   template d(i, j: C): auto = dI[(m.C + 2.C)*(i) + (j)]
