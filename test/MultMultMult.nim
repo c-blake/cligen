9f3a3f776459960d52fa1455dd95b84a176b53cf
Charles Blake <cb@cblake.net>
2019-01-16 16:47:21 -0500

    Add a 3rd level deep test program too, just for kicks.

---
+++ test/MultMultMult.nim
@@ -0,0 +1,46 @@
+proc demo(alpha=1, beta=2.0, verb=false, item="", files: seq[string]) =
+  ## demo entry point with varied, meaningless parameters.
+  echo "alpha:", alpha, " beta:", beta, " verb:", verb, " item:", repr(item)
+  for i, f in files: echo "args[", i, "]: ", repr(f)
+
+proc show(gamma=1, iota=2.0, verb=false, paths: seq[string]): int =
+  ## show entry point with varied, meaningless parameters.
+  echo "gamma:", gamma, " iota:", iota, " verb:", verb
+  for i, p in paths: echo "args[", i, "]: ", repr(p)
+  return 42
+
+proc whoa(zeta=1, eta=2.0, verb=false, names: seq[string]): int =
+  ## Another entry point; here we echoResult
+  echo "zeta:", zeta, " eta:", eta, " verb:", verb
+  for i, n in names: echo "args[", i, "]: ", repr(n)
+  return 12345
+
+proc nelly(hooves=4, races=9, verb=false, names: seq[string]): string =
+  ## Yet another entry point; here we block autoEcho
+  echo "hooves:", hooves, " races:", races, " verb:", verb
+  for i, n in names: echo "args[", i, "]: ", repr(n)
+  return "42"
+
+proc yikes(hooves=4, races=9, verb=false, names: seq[string]): string =
+  ## Yet another entry point; here we block autoEcho
+  echo "hooves:", hooves, " races:", races, " verb:", verb
+  for i, n in names: echo "args[", i, "]: ", repr(n)
+  return "42"
+
+when isMainModule:
+  import cligen
+  include cligen/mergeCfgEnv
+  cligenVersion = "0.0.1"
+  dispatchMultiGen([ "cobbler" ],
+                   [ yikes,
+                     mergeNames = @["MultiMulti", "apple", "cobbler", "yikes"]])
+  dispatchMultiGen([ "apple" ],
+                   [ demo, help = { "verb": "on=chatty, off=quiet" },
+                     mergeNames = @["MultiMulti", "apple", "demo"] ],
+                   [ show, cmdName="print", short = { "gamma": 'z' },
+                     mergeNames = @["MultiMulti", "apple", "show"] ],
+                   [ cobbler, stopWords = @[ "yikes" ],
+                     mergeNames = @["MultiMulti", "apple", "cobbler"] ])
+  dispatchMulti([ apple, stopWords = @["demo", "show", "cobbler" ] ],
+                [ whoa, echoResult=true ],
+                [ nelly, noAutoEcho=true ])
