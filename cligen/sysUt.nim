2c9351e74e41878c0c0b6eac5375b5c4097ce570
Charles Blake <cb@cblake.net>
2019-04-22 10:01:43 -0400

    Add `enumerate` for Python refugees/porting other CLI utility code.

--- cligen/sysUt.nim
+++ cligen/sysUt.nim
@@ -9,3 +9,21 @@ iterator maybePar*(parallel: bool, a, b: int): int =
     for i in `||`(a, b): yield i
   else:
     for i in a .. b: yield i
+
+import macros
+
+macro enumerate*(x: ForLoopStmt): untyped =
+  ## Generic enumerate macro
+  expectKind x, nnkForStmt
+  result = newStmtList()
+  result.add newVarStmt(x[0], newLit(0))
+  var body = x[^1]
+  if body.kind != nnkStmtList:
+    body = newTree(nnkStmtList, body)
+  body.add newCall(bindSym"inc", x[0])
+  var newFor = newTree(nnkForStmt)
+  for i in 1..x.len-3:
+    newFor.add x[i]
+  newFor.add x[^2][1]
+  newFor.add body
+  result.add newFor
