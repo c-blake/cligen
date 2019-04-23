f97c520cea870e77eb24d0ceda2b731d5ecf0c11
Charles Blake <cb@cblake.net>
2019-04-23 13:56:59 -0400

    Version-dependent toHashSet and initHashSet to make cligen back-portable to nim-0.19.2 & 0.19.4.

--- test/HashSets.nim
+++ test/HashSets.nim
@@ -1,14 +1,21 @@
 import sets
+when NimVersion <= "0.19.4":
+  proc toHashSet[A](keys: openArray[A]): HashSet[A] = toSet[A](keys)
+  proc initHashSet[A](initialSize=64): HashSet[A] = initSet[A](initialSize)
 
 type Color = enum red, green, blue
 
 proc demo(args: seq[string],
-  i1: HashSet[int8]  =initSet[int8]()  , I1: HashSet[int8]   = toSet([2'i8, 3]),
-  i2: HashSet[int16] =initSet[int16]() , I2: HashSet[int16]  = toSet([4'i16,5]),
-  u1: HashSet[uint8] =initSet[uint8]() , U1: HashSet[uint8]  = toSet([6'u8, 7]),
-  u2: HashSet[uint16]=initSet[uint16](), U2: HashSet[uint16] = toSet([8'u16,9]),
-  e : HashSet[Color] =initSet[Color]() , E : HashSet[Color]  = toSet([red,blue])
-): int =
+          i1: HashSet[int8]   = initHashSet[int8]() ,
+          I1: HashSet[int8]   = toHashSet([2'i8, 3]),
+          i2: HashSet[int16]  = initHashSet[int16](),
+          I2: HashSet[int16]  = toHashSet([4'i16,5]),
+          u1: HashSet[uint8]  = initHashSet[uint8](),
+          U1: HashSet[uint8]  = toHashSet([6'u8, 7]),
+          u2: HashSet[uint16] = initHashSet[uint16](),
+          U2: HashSet[uint16] = toHashSet([8'u16,9]),
+          e : HashSet[Color]  = initHashSet[Color](),
+          E : HashSet[Color]  = toHashSet([red,blue])): int =
   ## demo entry point with parameters of all basic types.
   echo "i1: ", i1, " I1: ", I1
   echo "i2: ", i2, " I2: ", I2
