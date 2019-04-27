3292f865cc2d86c6184c16b52782a4ba8450e465
Charles Blake <charlechaud@gmail.com>
2019-04-27 12:40:40 -0400

    Delete helpCase file.  CLI authors can only really override helpCase as used by generated dispatchers directly.  If they really hate my `argcvt.helpCase` for enums then they'll have to re-do both argParse/argHelp for enums (which they can do in some 30 line file somewhere to include between import & dispatch).

--- cligen/helpCase.nim
+++
@@ -1,7 +0,0 @@
-import strutils # replace
-
-proc helpCase*(inp: string, context: ClHelpContext = clSubCmd): string =
-  ##This is a string-to-string transformer hook to convert whatever the native
-  ##Nim code identifier casing is into a string for presentation to CLI users.
-  ##By default it converts snake_case to kebab-case.
-  result = inp.replace('_', '-')
