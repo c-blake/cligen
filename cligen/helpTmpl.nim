70472f26aef9f8acc0acb3fad896cbbe5d57742d
Charles Blake <charlechaud@gmail.com>
2019-05-31 08:57:15 -0400

    Factor clUseMulti a bit.

--- cligen/helpTmpl.nim
+++ cligen/helpTmpl.nim
@@ -1,11 +1,14 @@
 const clUse* = "$command $args\n$doc  Options(opt-arg sep :|=|spc):\n$options"
 const clUsage* = "Usage:\n  " & clUse   #Use is for dispatchMulti else Usage
 
-const clUseMulti* = """${doc}Usage:
-  $command {SUBCMD}  [sub-command options & parameters]
-where {SUBCMD} is one of:
-$subcmds
+const clUseMultiGeneral* = """
 $command {-h|--help} or with no args at all prints this message.
 $command --help-syntax gives general cligen syntax help.
 Run "$command {help SUBCMD|SUBCMD --help}" to see help for just SUBCMD.
 Run "$command help" to get *comprehensive* help.$ifVersion"""
+
+const clUseMulti* = """${doc}Usage:
+  $command {SUBCMD}  [sub-command options & parameters]
+where {SUBCMD} is one of:
+$subcmds
+""" & clUseMultiGeneral
