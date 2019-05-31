813353ffd7fe0b66d2c80093f5b02d6ef9beaea1
Charles Blake <charlechaud@gmail.com>
2019-05-31 11:18:27 -0400

    Do not update html if html exists and is newer than nim module.

--- doc.sh
+++ doc.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 for m in cligen cligen/parseopt3 cligen/argcvt; do
+  if [ docs/$(basename $m).html -nt $m.nim ]; then
+      echo $m html is up-to-date
+      continue
+  fi
   nim doc $m
   mv $(basename $m).html docs
 done
