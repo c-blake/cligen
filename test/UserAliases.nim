e54b4e366559292c6f6ec764fff475cb9f2b04c1
Charles Blake <charlechaud@gmail.com>
2019-06-04 05:34:52 -0400

    Rename test/*.cfg to test/*.cf to avoid confusion with per-module .nim.cfg's and add `UserAliases.nim`/`UserAliases.cf` test programs for CL user alias support. Also show proper (current) os.parseCmdLine quoting in `Version.cf` and update reference output.

---
+++ test/UserAliases.nim
@@ -0,0 +1,14 @@
+proc demo(al_pha=1, be_ta=2.0, verb=false, item="", args: seq[string]) =
+  ## demo entry point with varied, meaningless parameters with an alias system.
+  ## E.g., USERALIASES='"-Dk=-a9 -b3.0" "-DK=-Rk -v 1 2"' ./UserAliases -RK 3 4
+  ## or USERALIASES_CONFIG=UserAliases.cf ./UserAliases -RK 3 4
+  echo "alpha:", alpha, " beta:", beta, " verb:", verb, " item:", repr(item)
+  for i, arg in args: echo "positional[", i, "]: ", repr(arg)
+
+when isMainModule:
+  import cligen; include cligen/mergeCfgEnv
+  dispatch(demo, cmdName = "UserAliases",
+           help = { "al-pha" : "growth constant",
+                    "bet-a"  : "shrink target" },
+           alias = @[ ("Def", 'D', "define key=\"val ...\" alias"),
+                      ("Ref", 'R', "reference an alias") ] )
