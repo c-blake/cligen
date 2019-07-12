3f195209de4dab183ce193913115e7531b41ec59
Charles Blake <charlechaud@gmail.com>
2019-07-12 02:57:13 -0400

    Overhaul API to be easier using new type, `Abbrev`, and also add a simple test program in main mode.  Seems to work ok.

--- cligen/abbrev.nim
+++ cligen/abbrev.nim
@@ -32,93 +32,112 @@
 
 import strutils, algorithm, sets, tables, ./humanUt, ./textUt
 
-proc abbrev*(str, sep: string; mx, hd, tl: int): string {.inline.} =
-  ## Abbreviate str as str[0..<hd], sep, str[^tl..^1] only if str.len > mx.
-  if mx > 0 and str.len > mx:
-    str[0 ..< hd] & sep & str[^tl .. ^1]
+type Abbrev* = object
+  sep: string
+  mx*, hd, tl, sLen, m, h, t: int
+  optim: bool
+  abbOf: Table[string, string]
+
+proc isAbstract*(a: Abbrev): bool {.inline.} =
+  a.mx < 0 or a.hd < 0 or a.tl < 0
+
+proc update*(a: var Abbrev) {.inline.} =
+  ##Call this after any change to ``a.mx`` to update derived quantities.
+  a.m = a.mx; a.h = a.hd; a.t = a.tl
+  if not a.isAbstract: return
+  if a.hd == -1 and a.tl == -1:     #Both missing|auto: balanced tl-biased slice
+    a.h = (a.m - a.sLen) div 2
+    a.t = (a.m - a.sLen - a.h)
+  elif a.hd == -1: a.h = max(0, a.m - a.sLen - a.t) #Only missing 1; set oth to
+  elif a.tl == -1: a.t = max(0, a.m - a.sLen - a.h) #..remaining|0 if none left
+  a.h = min(a.h, a.m - a.sLen)
+  a.t = min(a.t, a.m - a.sLen)
+
+proc abbrev*(a: Abbrev, str: string): string {.inline.} =
+  ## Abbreviate ``str`` as ``str[0..<hd], sep, str[^tl..^1]`` only if
+  ## ``str.len > mx``.
+  if a.m > 0 and str.len > a.m:
+    str[0 ..< a.h] & a.sep & str[^a.t .. ^1]
   else:
     str
 
-proc parseAbbrevSetHdTl(mx, sLen: int; hd, tl: var int) {.inline.} =
-  if hd == -1 and tl == -1:     #Both missing or auto: balanced tl-biased slice
-    hd = (mx - sLen) div 2
-    tl = (mx - sLen - hd)
-  elif hd == -1: hd = max(0, mx - sLen - tl)  #Only missing one; set other to
-  elif tl == -1: tl = max(0, mx - sLen - hd)  #..remaining or zero if none left
-  hd = min(hd, mx - sLen)
-  tl = min(tl, mx - sLen)
-
-proc parseAbbrev*(s: string; mx: var int; sep: var string; hd, tl: var int) =
-  ##Parse comma-separated abbreviation spec ``s`` into ``mx``, ``sep``, ``hd``,
-  ##``tl``.  Non-numeric ``mx`` =>-1 => caller should re-invoke with correct mx.
-  ##Non-numeric|missing ``hd`` => ``mx-sep.len-tl`` Non-numeric or missing
-  ##``tl`` => ``mx-sep.len-hd``.  Non-num|missing both => ``hd=(mx-sep.len)/2;
-  ##tl=mx-sep.len-hd`` (which gives ``tl`` 1 more for odd ``mx-sep.len``).
+proc parseAbbrev*(s: string): Abbrev =
+  ##Parse comma-separated abbreviation spec [mx][,[hd][,[tl][,[sep]]]] ``s``
+  ##into ``Abbrev abb``.  Non-numeric ``mx`` => -1 => caller must set mx and
+  ##call ``update``.  Non-numeric|missing ``hd`` => ``mx-sep.len-tl``
+  ##Non-numeric or missing ``tl`` => ``mx-sep.len-hd``.  Non-num|missing both =>
+  ##``hd=(mx-sep.len)/2; tl=mx-sep.len-hd`` (which gives ``tl`` 1 more for odd
+  ##``mx-sep.len``).  ``mx <=0`` => various locally optimized ``uniqueAbbrevs``.
   if s.len == 0: return
   let cols = s.split(',')       #Leading/trailing whitespace in sep is used.
   if cols.len > 4: raise newException(ValueError, "bad abbrev spec: \""&s&"\"")
-  sep = if cols.len > 3: cols[3] else: "*"
-  if mx == 0: mx = parseInt(cols[0], -1)
-  hd = if cols.len > 1: parseInt(cols[1], -1) else: -1
-  tl = if cols.len > 2: parseInt(cols[2], -1) else: -1
-  if mx == -1: return           #Caller should re-invoke w/actual max
-  parseAbbrevSetHdTl(mx, sep.printedLen, hd, tl)
+  result.optim = s.startsWith("a")
+  result.mx = if cols.len > 0: parseInt(cols[0], -1) else: -1
+  result.hd = if cols.len > 1: parseInt(cols[1], -1) else: -1
+  result.tl = if cols.len > 2: parseInt(cols[2], -1) else: -1
+  result.sep = if cols.len > 3: cols[3] else: "*"
+  result.sLen = result.sep.printedLen
+  if result.mx != -1: result.update   #For -1 caller must call realize
 
-proc uniqueAbs(strs: openArray[string]; sep: string; mx, hd, tl: int): bool =
-  ## return true only if ``mx``, ``hd``, ``tl`` yields a set of unique
-  ## abbreviations for strs.
+proc uniqueAbs(a: Abbrev, strs: openArray[string]): bool =
+  ## Return true only if ``a`` yields a set of unique abbreviations for strs.
   var es = initHashSet[string]()
   for s in strs:                        #done when the first duplicate is seen
-    if es.containsOrIncl(abbrev(s, sep, mx, hd, tl)): return false
+    if es.containsOrIncl(a.abbrev s): return false
   return true
 
-proc smallestMaxSTUnique(strs: openArray[string]; mLen, sLen: int;
-                         sep: string; hd, tl: var int): tuple[m, h, t: int] =
-  var h2, t2: int
-  var lo = sLen + 1                     #Binary search on [sLen+1, mLen] for
-  var hi = mLen                         #..least result s.t. strs.uniqueAbs.
+proc minMaxSTUnique(a: var Abbrev, strs: openArray[string], ml: int) =
+  var a2 = a
+  var lo = a.sLen + 1                   #Binary search on [a.sLen+1, ml] for
+  var hi = ml                           #..least result s.t. a.uniqueAbs(strs).
   while hi > lo:
-    let m = (lo + hi) div 2
-    h2 = hd; t2 = tl                    #Assign to temporaries, not return hd,tl
-    parseAbbrevSetHdTl(m, sLen, h2, t2)
-    if strs.uniqueAbs(sep, m, h2, t2): hi = m     #m => unique: bracket lower
-    else: lo = m + 1                              #not unique: bracket higher
-  parseAbbrevSetHdTl(lo, sLen, hd, tl)  #fix up derived values
-  result = (lo, hd, tl)                 #Now lo == hi
+    a2.mx = (lo + hi) div 2; a2.update  #mid point
+    if a2.uniqueAbs(strs): hi = a2.mx   #mid => unique: bracket lower
+    else: lo = a2.mx + 1                #not unique: bracket higher
+  a.mx = lo; a.update                   #Now lo == hi; set mx & update derived
 
-proc smallestMaxSTUnique*(strs: openArray[string]; sep: string;
-                          hd, tl: var int, optim=false): int =
+proc realize*(a: var Abbrev, strs: openArray[string]) =
   ## Semi-efficiently find the smallest max such that ``strs`` can be uniquely
-  ## abbreviated by ``abbrev(s, mx, hd, tl)`` for all ``s`` in ``strs``.  If
-  ## ``optim`` is true, ignore any specified ``hd,tl`` and find ``hd,tl`` that
-  ## finds the minimum-minimum-maximum.
+  ## abbreviated by ``abbrev(s, mx, hd, tl)`` for all ``s`` in ``strs``.
+  a.update
   var mLen: int
   for s in strs: mLen = max(mLen, s.len)
-  let sLen = sep.printedLen
-  if mLen <= sLen + 1:
-    parseAbbrevSetHdTl(sLen + 1, sLen, hd, tl)
-    return sLen + 1
-  if optim:
+  if mLen <= a.sLen + 1:
+    a.mx = a.sLen + 1; a.update
+    return
+  if a.optim:
     var res: seq[tuple[m, h, t: int]]
     for h in 0..mLen:
-      var h2 = h; var t2 = -1
-      res.add strs.smallestMaxSTUnique(mLen, sLen, sep, h2, t2)
+      var a2 = a; a2.hd = h; a2.tl = -1
+      a2.minMaxSTUnique(strs, mLen)
+      res.add (a2.m, a2.h, a2.t)
     res.sort
-    hd = res[0].h
-    tl = res[0].t
-    result = res[0].m
+    a.hd = res[0].h; a.tl = res[0].t; a.mx = res[0].m
+    a.update
   else:
-    result = strs.smallestMaxSTUnique(mLen, sLen, sep, hd, tl).m
+    a.minMaxSTUnique(strs, mLen)
 
-proc smallestMaxSTUnique*[T](tab: Table[T, string]; sep: string;
-                             hd, tl: var int, optim=false): int =
+proc realize*[T](a: var Abbrev, tab: Table[T, string]) =
   ## Find smallest max s.t. abbrev unique over ``values`` of ``tab``.
+  if tab.len == 0 or a.mx >= 0: return
   var strs: seq[string]
   for v in tab.values: strs.add v
-  strs.smallestMaxSTUnique sep, hd, tl, optim
+  a.realize strs
 
 proc uniqueAbbrev*(strs: openArray[string]; sep: string; nWild=1): seq[string] =
   ## Return narrowest unique abbrevation set for ``strs`` given some number of
   ## wildcards (``sep``, probably ``*``), where both location and number of
   ## wildcards can vary from string to string.
   discard
+when isMainModule:
+  proc abb(abbr="", byLen=false, strs: seq[string]) =
+    var a = parseAbbrev(abbr)
+    a.realize strs
+    if byLen:
+      var sq: seq[string]
+      for s in strs: sq.add a.abbrev s
+      sq.sort(proc(a, b: string): int = cmp(a.len, b.len))
+      for s in sq: echo a.abbrev s
+    else:
+      for s in strs: echo a.abbrev s
+  import cligen; dispatch abb
