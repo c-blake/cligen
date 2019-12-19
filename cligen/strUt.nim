37088cdb56a3a75be682e1d6cdcef92c6e3ba0c3
Charles Blake <charlechaud@gmail.com>
2019-12-19 06:16:53 -0500

    Provide a compiling, though very broken, impl for non-int128 compilers.

--- cligen/strUt.nim
+++ cligen/strUt.nim
@@ -23,6 +23,8 @@ proc hashCB*(x: openArray[char]): uint64 =
     h3 += h2                  #     maybe short strs, better to optim tail cpy
   return h1 + h2 + h3         #     & finalize w/something like rotr(h2*h3,38).
 
+proc r8(p: pointer): uint64 {.inline.} = copyMem(addr result, p, 8)
+
 when defined(gcc) or defined(llvm_gcc) or defined(clang):
   #Machinery for accessing xpro=(hi^lo)(128 bit product) on gcc/clang backends.
   {.emit: "typedef unsigned __int128 NU128;".}
@@ -34,7 +36,14 @@ when defined(gcc) or defined(llvm_gcc) or defined(clang):
     var product = tm(A, B)
     return hi(product) xor lo(product)
 
-  proc r8(p: pointer): uint64 {.inline.} = copyMem(addr result, p, 8)
+  proc wyBlock(x: pointer): uint64 {.inline.} =
+    proc `+`(p:pointer,i:int):pointer {.inline.}=cast[pointer](cast[int](p)+%i)
+    const P1 = 0xe7037ed1a0b428db'u64; const P2 = 0x8ebc6af09c88c6e3'u64
+    const P3 = 0x589965cc75374cc3'u64; const P4 = 0x1d8e4e27c47d124f'u64
+    xpro(r8(x) xor P1, r8(x+8) xor P2) xor xpro(r8(x+16) xor P3,r8(x+24) xor P4)
+else:
+  func xpro(A: uint64, B: uint64): uint64 = #Xor of high & low 8B of product
+    A xor B
 
   proc wyBlock(x: pointer): uint64 {.inline.} =
     proc `+`(p:pointer,i:int):pointer {.inline.}=cast[pointer](cast[int](p)+%i)
@@ -42,20 +51,20 @@ when defined(gcc) or defined(llvm_gcc) or defined(clang):
     const P3 = 0x589965cc75374cc3'u64; const P4 = 0x1d8e4e27c47d124f'u64
     xpro(r8(x) xor P1, r8(x+8) xor P2) xor xpro(r8(x+16) xor P3,r8(x+24) xor P4)
 
-  proc hashWY*(x: openArray[char]): uint64 =
-    ## Wang Yi's nice hash passing SMHasher tests; *Unoptimized* for small keys
-    const P0 = 0xa0761d6478bd642f'u64; const P5 = 0xeb44accab455d165'u64
-    var h = 0'u64                     #Could seed/carry forward a hash here
-    let n = x.len
-    for i in countup(0, n - 32, 32):  #Block-wise loop helps speed
-      h = xpro(h xor P0, wyBlock(unsafeAddr x[i]))
-    if (n and 31) != 0:               #Copy any final partial block to temp buf
-      var q: array[32, char]
-      copyMem(unsafeAddr q[0], unsafeAddr x[n shr 5 shl 5], n and 31)
-      h = xpro(h xor P0, wyBlock(unsafeAddr q[0])) #WY SmallKey opt=>diff hshVal
-    return xpro(h, uint64(n) xor P5)
-  #NOTE 27+n/6 cycles has a BIG constant; Could explode into 31 cases like WY(or
-  #use ANY staged hashes for final 2,4,8,16,31B since len%32 is deterministic).
+proc hashWY*(x: openArray[char]): uint64 =
+  ## Wang Yi's nice hash passing SMHasher tests; *Unoptimized* for small keys
+  const P0 = 0xa0761d6478bd642f'u64; const P5 = 0xeb44accab455d165'u64
+  var h = 0'u64                     #Could seed/carry forward a hash here
+  let n = x.len
+  for i in countup(0, n - 32, 32):  #Block-wise loop helps speed
+    h = xpro(h xor P0, wyBlock(unsafeAddr x[i]))
+  if (n and 31) != 0:               #Copy any final partial block to temp buf
+    var q: array[32, char]
+    copyMem(unsafeAddr q[0], unsafeAddr x[n shr 5 shl 5], n and 31)
+    h = xpro(h xor P0, wyBlock(unsafeAddr q[0])) #WY SmallKey opt=>diff hshVal
+  return xpro(h, uint64(n) xor P5)
+#NOTE 27+n/6 cycles has a BIG constant; Could explode into 31 cases like WY(or
+#use ANY staged hashes for final 2,4,8,16,31B since len%32 is deterministic).
 
 proc startsWithI*(s, prefix: string): bool {.noSideEffect.} =
   ##Case insensitive variant of startsWith.
