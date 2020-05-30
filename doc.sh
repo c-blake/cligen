f4f6d61c756329444ba60912695c138b3302632c
jiro4989 <jiroron666@gmail.com>
2020-05-30 17:40:06 +0000

    remove unused

--- doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for m in cligen cligen/parseopt3 cligen/argcvt; do
-  if [ docs/$(basename $m).html -nt $m.nim ]; then
-      echo $m html is up-to-date
-      continue
-  fi
-  nim doc $m
-  mv $(basename $m).html docs
-done
