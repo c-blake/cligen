d76a547f3b04d82d1d4cd0cb4377cfd3a8480377
Charles Blake <charlechaud@gmail.com>
2020-06-10 07:50:24 -0400

    Overhaul to use/demonstrate new `forPath` (and be about 1.2x faster).

--- examples/newest.nim
+++ examples/newest.nim
@@ -1,6 +1,8 @@
-import heapqueue, cligen/[osUt, posixUt, statx], cligen
+import heapqueue, posix, cligen/[osUt, posixUt, dents, statx], cligen
 
-proc timeStamp(st: Statx, tim: char, dir: int): int =
+type TimePath = tuple[tm: int64, path: string]
+
+proc timeStamp(st: Statx, tim: char, dir: int): int64 {.inline.} =
   case tim
   of 'b': dir * getBirthTimeNsec(st)
   of 'a': dir * getLastAccTimeNsec(st)
@@ -9,62 +11,49 @@ proc timeStamp(st: Statx, tim: char, dir: int): int =
   of 'v': dir * max(getLastModTimeNsec(st), getCreationTimeNsec(st))
   else: 0
 
-proc doStat(path: string; st: var Statx; Deref, quiet: bool): bool =
-  if Deref:
-    if stat(path, st) == 0: return true
-    if lstat(path, st) == 0: return true     # lstat fallback if stat fails..
-  elif lstat(path, st) == 0: return true     #..or if re no deref requested.
-  if not quiet:
-    stderr.write("newest: \"", path, "\" missing\n")
+proc doStat(dfd: cint, path: string; nmAt: int, st: var Statx; Deref,
+            quiet: bool): bool {.inline.} =
+  if Deref:     # lstat if no deref requested or as fallback if stat fails.
+    if statxat(dfd, path[nmAt..^1], st, 0) == 0: return true
+    if lstatxat(dfd, path[nmAt..^1], st, 0) == 0: return true
+  elif lstatxat(dfd, path[nmAt..^1], st, 0) == 0: return true
+  if not quiet: stderr.write "newest: \"", path, "\" ", strerror(errno), '\n'
 
-type StrGen* = iterator(): string             ## A string generator iterator
-type TimePath = tuple[tm: int, path: string]
-
-iterator timePaths(paths: StrGen; time: string; Deref,quiet: bool): TimePath =
-  let tO = fileTimeParse(time)                #- or CAP=oldest
-  var st: Statx
-  for path in paths():
-    if not doStat(path, st, Deref, quiet):
-      continue
-    yield (timeStamp(st, tO.tim, tO.dir), path)
-
-iterator newest*(paths:StrGen, n=1, time="m", Deref=false, quiet=false):string =
-  ## Yield at most ``n`` newest files in ``time``-order where ``time`` can be
-  ## [-][BAMCV]* for Birth, Access, Mod, Ctime, Version=max(M,C); Optional "-"
-  ## means oldest not newest.  Eg., quietly print oldest-by-mtime 2 DIR entries:
-  ##
-  ## .. code-block ::
-  ##   iterator gen(): string =
-  ##     for e in os.walkDir(DIR, false): yield e.path
-  ##   for e in newest(gen, 2, "-m"): echo e
-  var hq = newHeapQueue[TimePath]()         # min-heap with hq[0]=min
-  for tp in timePaths(paths, time, Deref, quiet):
-    if hq.len < n  : hq.push(tp)            # build initial heap
-    elif tp > hq[0]: discard hq.replace(tp) # time > curr min => replace
-  while hq.len > 0:                         # hq now has top n times.
-    let e = hq.pop()                        # pop min elt will yield in..
-    yield e.path                            #..the specified-time order.
-
-proc printNewest*(n=1, time="m", recurse=1,chase=false,Deref=false, quiet=false,
-                  outEnd="\n", file="", delim='\n', paths: seq[string]) =
-  ## Print ``<=n`` newest paths ended by ``outEnd`` in ``time``-order {
-  ## [-][bamcv].* for Birth, Access, Mod, Ctime, Version=max(MC); optional '-'
-  ## means oldest instead of newest }.  Examined paths are UNION of ``paths`` +
+proc printNewest*(n=1, time="m", recurse=1, chase=false, Deref=false,
+                  quiet=false, xdev=false, outEnd="\n", file="", delim='\n',
+                  paths: seq[string]) =
+  ## Echo ``<=n`` newest paths ended by ``outEnd`` in ``time`` order [-][bamcv]
+  ## for Birth, Access, Mod, Ctime, Version=max(MC); Optional '-' (or CAPITAL)
+  ## means oldest instead of newest.  Examined paths are UNION of ``paths`` +
   ## optional ``delim``-delimited input ``file`` (stdin if "-"|if "" & stdin is
   ## not a tty).  Eg., ``find -type f|newest -t-m`` prints the m-oldest file.
-  for e in newest(recEntries(both(paths, fileStrings(file, delim)),
-                             follow=chase, maxDepth=recurse),
-                  n, time, Deref, quiet):
-    stdout.write e, outEnd
+  let tO = fileTimeParse(time)                  #- or CAPITAL=oldest
+  let it = both(paths, fileStrings(file, delim))
+  var q  = newHeapQueue[TimePath]()             # min-heap with q[0]=min
+  for root in it():
+    if root.len == 0: continue                  # skip any improper inputs
+    forPath(root,recurse,false,chase,xdev, depth,path,dfd,nmAt,ino,dt,lst,dst):
+      if dt != DT_UNKNOWN:                      # unknown here => disappeared
+        if (dt==DT_LNK and Deref and doStat(dfd,path,nmAt,lst,Deref,quiet)) or
+           lst.stx_nlink != 0 or doStat(dfd,path,nmAt,lst,Deref,quiet):
+          let tp = (timeStamp(lst, tO.tim, tO.dir), path)
+          if q.len < n  : q.push(tp)
+          elif tp > q[0]: discard q.replace(tp)
+    do: discard
+    do: discard
+    do: recFailDefault("newest")
+  while q.len > 0:                              # q now has top n times; Print
+    stdout.write q.pop().path, outEnd           #..in the specified-time order.
 
 when isMainModule:  # Exercise this with an actually useful CLI wrapper.
   dispatch(printNewest, cmdName="newest",
-           help = { "n"     : "number of 'newest' files",
-                    "time"  : "timestamp to compare ([-][bamcv].*)",
+           help = { "n"      : "number of 'newest' files",
+                    "time"   : "timestamp to compare ([-][bamcv].*)",
                     "recurse": "recurse n-levels on dirs; 0:unlimited",
-                    "chase" : "chase symlinks to dirs in recursion",
-                    "Deref" : "dereference symlinks for file times",
-                    "quiet" : "suppress file access errors",
-                    "outEnd": "output record terminator",
-                    "file"  : "optional input (\"-\"|!tty=stdin)",
-                    "delim" : "input file record delimiter" })
+                    "chase"  : "chase symlinks to dirs in recursion",
+                    "xdev"   : "block recursion across device boundaries",
+                    "Deref"  : "dereference symlinks for file times",
+                    "quiet"  : "suppress file access errors",
+                    "outEnd" : "output record terminator",
+                    "file"   : "optional input (\"-\"|!tty=stdin)",
+                    "delim"  : "input file record delimiter" })
