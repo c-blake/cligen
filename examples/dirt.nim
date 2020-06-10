4944d653173e77a867eb12120b1eee2f150dd466
Charles Blake <charlechaud@gmail.com>
2020-06-10 15:15:10 -0400

    Add more complex example using all forPath clauses than the simpler recursive remove in `examples/rr.nim`.

---
+++ examples/dirt.nim
@@ -0,0 +1,52 @@
+# NOTE: Needs `devel` / >= 1.4.0 for `HeapQueue[T].find`.
+import heapqueue, posix, strformat, cligen, cligen/[dents, posixUt, statx]
+
+proc setMTime*(dfd: cint; path: string; m0, m1: StatxTs;
+               verb: File=nil, err=stderr, dryRun=false): int =
+  ## Set the file m1)odification time of ``(dfd,path[nmAt..^1])`` only if not
+  ## equal to the original ``m0`` times with typical command utility ``verb``,
+  ## ``err``, ``dryRun`` parameters, returning if the call must/did occur.
+  if m0 == m1: return 0
+  result = 1
+  let omit = Timespec(tv_sec: 0.Time, tv_nsec: UTIME_OMIT)
+  var ftms = [ omit, toTimespec(m1)]
+  verb.log &"futimens({dfd}({path}), [OMIT, {$ftms[1]}])\n"
+  if not dryRun and futimens(dfd, ftms) != 0:
+    err.log &"futimens({dfd}({path}): {strerror(errno)}\n"
+
+proc dirt*(roots: seq[string], verbose=false, quiet=false, dryRun=false,
+           xdev=false): int =
+  ## Set mtimes of dirs under ``roots`` to mtime of its newest kid.  This makes
+  ## directory mtimes "represent" content age at the expense of erasing evidence
+  ## of change which can be nice for time-sorted ls in some archival file areas.
+  if roots.len == 0:  # For safety, do nothing if user specifies empty `paths`
+    return
+  let verb = if dryRun or verbose: stdout else: nil
+  let err  = if quiet: nil else: stderr
+  var n    = 0
+  for root in roots:
+    var dirs = @[initHeapQueue[int64]()]      # HeapQueue.pop is *MINIMUM*
+    forPath(root,0,lstats=true,false,xdev, depth,path,dfd,nmAt,ino,dt,lst,dst):
+      if dt != DT_LNK:                        # Always:
+        dirs[^1].push -toInt64(lSt.stx_mtime) #   Track max age
+    do:                                       # Pre-recurse:
+      dirs.add initHeapQueue[int64]()         #   Add new queue for kid
+      let dmt = lSt.stx_mtime                 #   Save old mtime
+    do:                                       # Post-recurse:
+      if dirs.len > 0:
+        if dirs[^1].len > 0:                  #   Deepest queue non-empty
+          let kidTm = dirs[^1].pop            #   Get & use max kid time stamp
+          n += setMTime(dfd, path, dmt, toStatxTs(-kidTm), verb, err, dryRun)
+          if dirs.len > 1:                    #   ASSUME setMTime SUCCEEDS
+            dirs[^2].del  dirs[^2].find(-toInt64(dmt)) #XXX BST/BTreeQ 4big dirs
+            dirs[^2].push kidTm               #   reflect dmt -> kidTm in parent
+        discard dirs.pop                      #   discard kid queue
+    do: recFailDefault("dirt")                # Cannot recurse
+  return min(n, 255)
+
+when isMainModule:
+  dispatch(dirt, short = { "dry-run": 'n' }, help = {
+             "verbose" : "print `utimes` calls as they happen",
+             "quiet"   : "suppress most OS error messages",
+             "dry-run" : "only print what system calls are needed",
+             "xdev"    : "block recursion across device boundaries" })
