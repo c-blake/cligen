3cf269c1d8dda1490fa23c7bf7da4e8b1e77f762
Charles Blake <charlechaud@gmail.com>
2020-08-09 07:02:22 -0400

    Add another example program...Sorta like `inotifywait` *but* due to a different CL interface idea, can dispatch to handler commands without worrying about either NUL ('\0') term *or* shell quoting of odd file names.  Eg., `dirq -dfoo rm -f --` just clears a queue when populated (assuming the invoking process has permission).

---
+++ examples/dirq.nim
@@ -0,0 +1,60 @@
+import posix/inotify, posix, cligen/[sysUt, posixUt], cligen
+
+type Event* = enum
+  inAccess    ="access"   , inAttrib ="attrib"    , inModify    ="modify"      ,
+  inOpen      ="open"     , inCloseWr="closeWrite", inCloseNoWr ="closeNoWrite",
+  inMovedFrom ="movedFrom", inMovedTo="movedTo"   , inMoveSelf  ="moveSelf"    ,
+  inCreate    ="create"   , inDelete ="delete"    , inDeleteSelf="deleteSelf"
+
+proc mask(es: set[Event]): uint32 =
+  template `|=`(r, f) = r = r or f
+  for e in es:
+    case e
+    of inAccess    : result |= IN_ACCESS    
+    of inAttrib    : result |= IN_ATTRIB    
+    of inModify    : result |= IN_MODIFY    
+    of inOpen      : result |= IN_OPEN      
+    of inCloseWr   : result |= IN_CLOSE_WRITE
+    of inCloseNoWr : result |= IN_CLOSE_NOWRITE
+    of inMovedFrom : result |= IN_MOVED_FROM 
+    of inMovedTo   : result |= IN_MOVED_TO   
+    of inMoveSelf  : result |= IN_MOVE_SELF  
+    of inCreate    : result |= IN_CREATE    
+    of inDelete    : result |= IN_DELETE    
+    of inDeleteSelf: result |= IN_DELETE_SELF
+
+iterator dqueue*(dir: string; events={inMovedTo, inCloseWr}):
+    tuple[name: ptr char; len: int] =
+  ## Set up event watches on dir & forever yield NUL-terminated (ptr char,len).
+  if chdir(dir) == -1:
+    raise newException(OSError, "chdir(\"" & dir & "\")")
+  let fd = cint(inotify_init())
+  if fd == -1:
+    raise newException(OSError, "inotify_init")
+  if inotify_add_watch(fd, dir, events.mask or IN_ONLYDIR) == -1:
+    raise newException(OSError, "inotify_add_watch")
+  var evs = newSeq[byte](8192)
+  while (let n = read(fd, evs[0].addr, 8192); n) > 0:
+    for ev in inotify_events(evs[0].addr, n):
+      yield (ev[].name.addr, int(ev[].len))
+
+when isMainModule:
+  proc dirq(events={inMovedTo, inCloseWr}; dir="."; wait=false;
+            cmdPrefix: seq[string]): int =
+    ## Wait for *events* to occur on *dir* and then run *cmdPrefix* **NAME**
+    ## where **NAME** is the filename (not full path) delivered with the event.
+    ## Default events are any writable fd-close on files in *dir* or rename
+    ## into *dir* (typically signals that **NAME** is ready as an input file).
+    let n   = cmdPrefix.len                     # index of a new last slot
+    let cmd = allocCStringArray(cmdPrefix & "") # setup ready-to-exec buffer
+    if not wait:
+      signal(SIGCHLD, reapAnyKids)              # Block zombies
+    for (name, nmLen) in dqueue(dir, events):
+      if nmLen > 0 or name != nil:
+        cmd[n] = name                           # Poke ptr char into slot and..
+        discard cmd.system(wait)                #..run command, maybe in bkgd
+
+  dispatch(dirq, help = { "events": "inotify event types to use; " &
+                                    "Giving \"x\" lists all",
+                          "dir"   : "directory to watch",
+                          "wait"  : "wait4(kid) until re-launch" })
