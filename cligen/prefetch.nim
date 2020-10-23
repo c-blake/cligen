1f25ac7e19115a13a7fb5dcaca20b268629316c7
Charles Blake <charlechaud@gmail.com>
2020-10-23 09:22:24 -0400

    Slight improvement to get memory system loading the next line/token while the current one is being further processed.

---
+++ cligen/prefetch.nim
@@ -0,0 +1,19 @@
+type
+  PrefetchRW* {.size: cint.sizeof.} = enum
+    pfRead  = 0
+    pfWrite = 1
+  PrefetchKind* {.size: cint.sizeof.} = enum
+    pfEvictAll  = 0
+    pfEvictL1   = 1
+    pfEvictL2   = 2
+    pfEvictNone = 3
+
+when defined(gcc):                              #XXX or defined(clang) etc.
+  {.passc: "-march=native".}
+  proc prefetch*(data: pointer, rw=pfRead, kind=pfEvictNone) {.
+    importc: "__builtin_prefetch", nodecl .}
+  proc prefetchw*(data: pointer, kind=pfEvictNone) {.inline.} =
+    prefetch(data, pfWrite, kind)
+else:
+  proc prefetch*(data: pointer, rw=pfRead, kind=pfEvictNone) {.inline.} =discard
+  proc prefetchw*(data: pointer, kind=pfEvictNone) {.inline.} = discard
