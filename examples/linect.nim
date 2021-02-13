43a4d030fc9038e3273d3ac80268d76d1d453970
Charles Blake <charlechaud@gmail.com>
2021-02-13 14:56:05 -0500

    Handle file system errors somewhat.

--- examples/linect.nim
+++ examples/linect.nim
@@ -7,20 +7,21 @@ proc doLines(sub: ptr int, c: int, part: MSlice) =
   sub[] = tot                           #..shadow w/local updated only once.
 
 proc lc(path: string, n: int, untermOk=true, pparts=false): int =
-  var (mf, parts) = n.nSplit(path)      # file & sections
-  var sub = newSeq[int](parts.len)      # subtotals
-  for c, part in parts:
-    spawn doLines(sub[c].addr, c, part)
-  sync()                                # all finished
-  if pparts:
-    stdout.write sub.len
-    for i in sub: stdout.write " ", i
-    stdout.write '\n'
-  result = sub.sum
-  if (not untermOk) and mf.len > 0 and  # unterm debatable
-     cast[ptr char](mf.mem +! (mf.len - 1))[] != '\n':
-    result.dec
-  mf.close
+  if (var (mf, parts) = n.nSplit(path); mf) != nil:     # file & sections
+    var sub = newSeq[int](parts.len)                    # subtotals
+    for c, part in parts:
+      spawn doLines(sub[c].addr, c, part)
+    sync()                                # all finished
+    if pparts:
+      stdout.write sub.len
+      for i in sub: stdout.write " ", i
+      stdout.write '\n'
+    result = sub.sum
+    if (not untermOk) and mf.len > 0 and  # unterm debatable
+       cast[ptr char](mf.mem +! (mf.len - 1))[] != '\n':
+      result.dec
+    mf.close
+  else: stderr.write "linect: \"",path,"\" missing/irregular\n"
 
 proc linect(paths: seq[string], nThread=0, untermOk=false, parts=false) =
   ## Like `wc -l` using `nThreads` to (maybe) hasten big files to inspect
