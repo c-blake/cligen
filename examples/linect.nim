4406da545d6db210e4f6e31d75183c04c9692893
Charles Blake <charlechaud@gmail.com>
2021-02-13 15:10:31 -0500

    Meh..busy wait (with 0ms) sucks and 1ms turns out to be long in the context of just counting lines.  So, ditch `threadpool` entirely and `createThread` and `joinThreads` manually.  In this case, it's barely more complex..like 2 or 3 lines of code, really.

--- examples/linect.nim
+++ examples/linect.nim
@@ -1,17 +1,23 @@
-#nim c --gc:arc --threads:on -d:threadpoolWaitMs=0 -d:danger linect
 import math, mfile, mslice, osproc, threadpool, cligen
 
-proc doLines(sub: ptr int, c: int, part: MSlice) =
-  var tot = sub[]                       # sub[] is part of a dense array
-  for s in part.mSlices('\n'): tot.inc  #..ENSURING cache thrashing.  So,
-  sub[] = tot                           #..shadow w/local updated only once.
+type ThrDat = tuple[part: ptr MSlice, subp: ptr int]
+var thrs: seq[Thread[ThrDat]]
+
+proc work(td: ThrDat) {.thread.} =
+  var tot = td.subp[]                   # subp[] is part of a dense array
+  for s in td.part[].mSlices('\n'):     #..ENSURING cache thrashing.  So,
+    tot.inc                             #..shadow w/local updated only once.
+  td.subp[] = tot                 
 
 proc lc(path: string, n: int, untermOk=true, pparts=false): int =
   if (var (mf, parts) = n.nSplit(path); mf) != nil:     # file & sections
     var sub = newSeq[int](parts.len)                    # subtotals
-    for c, part in parts:
-      spawn doLines(sub[c].addr, c, part)
-    sync()                                # all finished
+    if n > 1:                           # add mf.len > 65536|something?
+      for i, part in parts:
+        createThread thrs[i], work, (parts[i].addr, sub[i].addr)
+      joinThreads thrs                  # all finished
+    else:
+        work (parts[0].addr, sub[0].addr)
     if pparts:
       stdout.write sub.len
       for i in sub: stdout.write " ", i
@@ -28,6 +34,7 @@ proc linect(paths: seq[string], nThread=0, untermOk=false, parts=false) =
   ## workload distribution when `parts` is true.  For example:
   ##   `for n in {1..16}; do printf "$n "; linect -pn$n cligen.nim; done`
   let n = if nThread == 0: countProcessors() else: nThread
+  thrs.setLen n                         # allocate `thrs` & `cnts`
   var total = 0
   for path in paths:
     let m = lc(path, n, untermOk, parts)
