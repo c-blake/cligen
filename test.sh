a24c872943fab0d1d97662ca479021ae6d5e61c0
Charles Blake <charlechaud@gmail.com>
2021-02-18 04:30:23 -0500

    Simplify `test.sh` in light of `CLIGEN_WIDTH`. (`COLUMNS` likely unneeded, but eh.)

--- test.sh
+++ test.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-# If you cannot|refuse to run in an 80 column terminal|with no ctty at all then
-#   x=$COLUMNS; stty columns 80; ./test.sh; stty columns $x
-# will likely match reference output.  (NOTE: Final stty clobbers test.sh $?)
-# But this script is really only for cligen developers.
 exec < /dev/null
-export COLUMNS=80
-export CLIGEN=/dev/null
+export COLUMNS="80" CLIGEN_WIDTH="80" CLIGEN=/dev/null
 rm -rf $HOME/.cache/nim/*
 v="--verbosity:1"
 h="--hint[Processing]=off"
