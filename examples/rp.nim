ecbdee3f818faf615e85e6f1d32f2eec9f5b1fe9
Charles Blake <charlechaud@gmail.com>
2021-12-28 11:12:25 -0500

    Rename `--test` to `--where` to be more like `nio qry`/SQL.

--- examples/rp.nim
+++ examples/rp.nim
@@ -9,7 +9,7 @@ proc toDef(fields, delim, genF: string): string =
   var s: seq[MSlice]
   var nms: HashSet[string]
   sep.split(row, s) # No maxSplit - define every field; Could infer it from the
-  for j, f in s:    #..highest referenced field with a `test` & `stmts` parse.
+  for j, f in s:    #..highest referenced field with a `where` & `stmts` parse.
     let nm = optionNormalize(genF % [ $f ])   # Prevent duplicate def errors..
     if nm notin nms:                          #..and warn users about collision.
       result.add "const " & nm & " {.used.} = " & $j & "\n" #XXX strop, too?
@@ -17,20 +17,20 @@ proc toDef(fields, delim, genF: string): string =
     else:
       stderr.write "rp: WARNING: ", nm, " collides with earlier field\n"
 
-proc rp(prelude="", begin="", test="true", stmts:seq[string], epilog="",
+proc rp(prelude="", begin="", where="true", stmts:seq[string], epilog="",
         fields="",genF="$1",nim="",run=true,args="",verbose=0,outp="/tmp/rpXXX",
         input="/dev/stdin", delim="white", uncheck=false, maxSplit=0): int =
-  ## Gen+Run *prelude*,*fields*,*begin*,*test*,*stmts*,*epilog* row processor
-  ## against *input*.  Defined within *test* & every *stmt* are:
+  ## Gen+Run *prelude*,*fields*,*begin*,*where*,*stmts*,*epilog* row processor
+  ## against *input*.  Defined within *where* & every *stmt* are:
   ##   *s[fieldIdx]* & *row* give `MSlice` (*$* to get a Nim *string*)
   ##   *i(fieldIdx)* gives a Nim int, *f(fieldIdx)* a Nim float.
   ##   *nf* & *nr* (like *AWK*);  NOTE: *fieldIdx* is **0-origin**.
   ## A generated program is left at *outp*.nim, easily copied for "utilitizing".
   ## If you know *AWK* & Nim, you can learn *rp* PRONTO.  Examples (need data):
   ##   **rp 'echo s[1]," ",s[0]'**                     # Swap field order
-  ##   **rp -t'nr mod 100==0' 'echo row'**             # Print each 100th row
+  ##   **rp -w'nr mod 100==0' 'echo row'**             # Print each 100th row
   ##   **rp -b'var t=0' t+=nf -e'echo t'**             # Print total field count
-  ##   **rp -b'var t=0' -t'i(0)>0' t+=0.i -e'echo t'** # Total >0 field0 ints
+  ##   **rp -b'var t=0' -w'i(0)>0' t+=0.i -e'echo t'** # Total >0 field0 ints
   ##   **rp -p'import stats' -b'var r: RunningStat' 'r.push 0.f' -e'echo r'**
   ##   **rp 'let x=f(0)' 'echo (1+x)/x'**              # cache field 0 parse
   ##   **rp -d, -fa,b,c 'echo s[a],f(b)+i(c).float'**  # named fields (CSV)
@@ -56,8 +56,8 @@ $4 # {begin}
   for row in mSlices("$5", eat='\0'): # {input} mmap|slices from stdio
 ${6}rpNmSepOb.split(row, s, $7) # {maxSplit}
     let nf {.used.} = s.len
-    if $8: # {test} auto ()s?
-""" % [prelude, fields, delim, indent(begin, 2), input, check, $maxSplit, test]
+    if $8: # {where} auto ()s?
+""" % [prelude, fields, delim, indent(begin, 2), input, check, $maxSplit, where]
   for i, stmt in stmts:
     program.add "      " & stmt & " # {stmt" & $i & "}\n"
   if stmts.len == 0:
@@ -82,8 +82,8 @@ when isMainModule:
   import cligen; include cligen/mergeCfgEnv
   dispatch rp, help={"prelude" : "Nim code for prelude/imports section",
                      "begin"   : "Nim code for begin/pre-loop section",
-                     "test"    : "Nim code for row inclusion",
-                     "stmts"   : "Nim stmts to run under test",
+                     "where"   : "Nim code for row inclusion",
+                     "stmts"   : "Nim stmts to run guarded by `where`",
                      "epilog"  : "Nim code for epilog/end loop section",
                      "fields"  : "`delim`-sep field names (match row0)",
                      "genF"    : "make field names from this fmt; eg c$1",
