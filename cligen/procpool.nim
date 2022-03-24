34e0d74143bc8a08d004555e4d792619d9bd0e1d
Charles Blake <charlechaud@gmail.com>
2022-03-24 09:39:10 -0400

    The `initProcPool` param `frames` must be correctly paired with a reply evaluator anyway.  So, do not default param & rearrange less OS-specific code to pair up frame/eval to bottom of the file (in prep for better documentation/explanation).

--- cligen/procpool.nim
+++ cligen/procpool.nim
@@ -30,39 +30,6 @@ proc request*(pp: ProcPool, kid: int, buf: pointer, len: int) =
 proc close*(pp: ProcPool, kid: int) =
   discard pp.kids[kid].fd0.close
 
-# `frames*` are early in the module to default `frames` in `initProcPool`, but
-# they logically pair with `eval*` at the end of the module.
-proc frames0term*(f: var Filter): iterator(): MSlice =
-  ## An output frames iterator for workers writing '\0'-terminated results.
-  let f = f.addr # Seems to relate to nimWorkaround14447; Can `lent`|`sink` fix?
-  result = iterator(): MSlice = # NOTE: results must be <= bufSz
-    let nRd = read(f.fd1, f.buf[f.off].addr, f.buf.len - f.off)
-    if nRd > 0:
-      let ms = MSlice(mem: f.buf[0].addr, len: f.off + nRd)
-      let eoms = cast[uint](ms.mem) + cast[uint](ms.len)
-      f.off = 0
-      for s in ms.mSlices('\0'):
-        let eos = cast[uint](s.mem) + cast[uint](s.len)
-        if eos < eoms and cast[ptr char](eos)[] == '\0':
-          yield s
-        else:
-          f.off = s.len
-          moveMem f.buf[0].addr, s.mem, s.len
-    else:
-      f.done = true
-
-proc framesLenPfx*(f: var Filter): iterator(): MSlice =
-  ## An output frames iterator for wrk procs writing [int, value] results.
-  let f = f.addr # Seems to relate to nimWorkaround14447; Can `lent`|`sink` fix?
-  result = iterator(): MSlice = # NOTE: must cp to other mem before next call.
-    var n = 0
-    if (let nHd = read(f.fd1, n.addr, n.sizeof); nHd == n.sizeof):
-      f.buf.setLen n #XXX below needs a loop for big `n` in case of EINTR.
-      if (let nRd = read(f.fd1, f.buf[0].addr, n); nRd > 0):
-        yield MSlice(mem: f.buf[0].addr, len: n)
-      else: f.done = true
-    else: f.done = true
-
 # NOTE: A when(Windows) PR doing CreatePipe/CreateProcess is welcome.
 proc initFilter(work: proc(), bufSz: int): Filter {.inline.} =
   var fds0, fds1: array[2, cint]
@@ -87,7 +54,7 @@ proc initFilter(work: proc(), bufSz: int): Filter {.inline.} =
     discard close(fds0[0])
     discard close(fds1[1])
 
-proc initProcPool*(work: proc(); frames = frames0term; jobs = 0;
+proc initProcPool*(work: proc(); frames: Frames; jobs = 0;
                    bufSize = 16384): ProcPool {.noinit.} =
   result.kids.setLen (if jobs == 0: countProcessors() else: jobs)
   FD_ZERO result.fdset
@@ -127,18 +94,17 @@ iterator finalReplies*(pp: var ProcPool): MSlice =
             discard waitpid(pp.kids[i].pid, st, 0)    # Accum CPU to par;No zomb
             n.dec
 
-template eval0term*(pp, reqGen, onReply: untyped) =
-  ## Use `pp=initProcPool(wrk)` & this to send Nim strings made by `reqGen` (any
-  ## iterator expr) to `wrk` stdin as 0-terminated & pass stdout-emitted framed
-  ## replies to `onReply`.  `examples/only.nim` is a full demo.
-  var i = 0
-  for req in reqGen:
-    pp.request(i, cstring(req), req.len + 1)    # keep NUL; Let full pipe block
-    i = (i + 1) mod pp.len
-    if i + 1 == pp.len:                         # At the end of each req cycle
-      for rep in pp.readyReplies: onReply(rep)  #..handle ready replies.
-  for i in 0 ..< pp.len: pp.close(i)            # Send EOFs
-  for rep in pp.finalReplies: onReply(rep)      # Handle final replies
+proc framesLenPfx*(f: var Filter): iterator(): MSlice =
+  ## An output frames iterator for wrk procs writing [int, value] results.
+  let f = f.addr # Seems to relate to nimWorkaround14447; Can `lent`|`sink` fix?
+  result = iterator(): MSlice = # NOTE: must cp to other mem before next call.
+    var n = 0
+    if (let nHd = read(f.fd1, n.addr, n.sizeof); nHd == n.sizeof):
+      f.buf.setLen n #XXX below needs a loop for big `n` in case of EINTR.
+      if (let nRd = read(f.fd1, f.buf[0].addr, n); nRd > 0):
+        yield MSlice(mem: f.buf[0].addr, len: n)
+      else: f.done = true
+    else: f.done = true
 
 template evalLenPfx*(pp, reqGen, onReply: untyped) =
   ## Use `pp=initProcPool(wrk)` & this to send strings made by `reqGen` (any
@@ -149,6 +115,38 @@ template evalLenPfx*(pp, reqGen, onReply: untyped) =
     n = req.len
     discard pp.kids[i].fd0.write(n.addr, n.sizeof)
     discard pp.kids[i].fd0.write(req[0].unsafeAddr, n) # Let full pipe block
+    i = (i + 1) mod pp.len
+    if i + 1 == pp.len:                         # At the end of each req cycle
+      for rep in pp.readyReplies: onReply(rep)  #..handle ready replies.
+  for i in 0 ..< pp.len: pp.close(i)            # Send EOFs
+  for rep in pp.finalReplies: onReply(rep)      # Handle final replies
+
+proc frames0term*(f: var Filter): iterator(): MSlice =
+  ## An output frames iterator for workers writing '\0'-terminated results.
+  let f = f.addr # Seems to relate to nimWorkaround14447; Can `lent`|`sink` fix?
+  result = iterator(): MSlice = # NOTE: results must be <= bufSz
+    let nRd = read(f.fd1, f.buf[f.off].addr, f.buf.len - f.off)
+    if nRd > 0:
+      let ms = MSlice(mem: f.buf[0].addr, len: f.off + nRd)
+      let eoms = cast[uint](ms.mem) + cast[uint](ms.len)
+      f.off = 0
+      for s in ms.mSlices('\0'):
+        let eos = cast[uint](s.mem) + cast[uint](s.len)
+        if eos < eoms and cast[ptr char](eos)[] == '\0':
+          yield s
+        else:
+          f.off = s.len
+          moveMem f.buf[0].addr, s.mem, s.len
+    else:
+      f.done = true
+
+template eval0term*(pp, reqGen, onReply: untyped) =
+  ## Use `pp=initProcPool(wrk)` & this to send Nim strings made by `reqGen` (any
+  ## iterator expr) to `wrk` stdin as 0-terminated & pass stdout-emitted framed
+  ## replies to `onReply`.  `examples/only.nim` is a full demo.
+  var i = 0
+  for req in reqGen:
+    pp.request(i, cstring(req), req.len + 1)    # keep NUL; Let full pipe block
     i = (i + 1) mod pp.len
     if i + 1 == pp.len:                         # At the end of each req cycle
       for rep in pp.readyReplies: onReply(rep)  #..handle ready replies.
