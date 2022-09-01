07bff7245265a19c6041995468cfcd403909ef97
Charles Blake <charlechaud@gmail.com>
2022-09-01 12:27:28 -0400

    Add some non-danger mode safeguards.

--- cligen/mslice.nim
+++ cligen/mslice.nim
@@ -53,9 +53,15 @@ template `^^`(s, i: untyped): untyped =
   (when i is BackwardsIndex: s.len - int(i) else: int(i))
 
 proc `[]`*(ms: MSlice, i: int): char {.inline.} =
+  when not defined(danger):
+    if i >= ms.len:
+      raise newException(IndexDefect, formatErrorIndexBound(i, ms.len))
   ms.mem.toCstr[i]
 
 proc `[]=`*(ms: MSlice, i: int, c: char) {.inline.} =
+  when not defined(danger):
+    if i >= ms.len:
+      raise newException(IndexDefect, formatErrorIndexBound(i, ms.len))
   cast[ptr char](ms.mem +! i)[] = c
 
 proc `[]`*[T, U: Ordinal](s: MSlice, x: HSlice[T, U]): MSlice {.inline.} =
