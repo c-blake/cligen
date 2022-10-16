21756b6d83286cd0dcd82b5abdde916e4ec42ed5
Charles Blake <charlechaud@gmail.com>
2022-10-16 15:42:59 -0400

    Between this https://forum.nim-lang.org/t/5992#37093 and certain cases of nullary template calls only getting fixed very, very recently it seems a better idea to leave ()s in the calls.

--- cligen/mslice.nim
+++ cligen/mslice.nim
@@ -53,19 +53,18 @@ template `^^`(s, i: untyped): untyped =
   (when i is BackwardsIndex: s.len - int(i) else: int(i))
 
 template BadIndex: untyped =
-  when declared(IndexDefect): IndexDefect
-  else: IndexError
+  when declared(IndexDefect): IndexDefect else: IndexError
 
 proc `[]`*(ms: MSlice, i: int): char {.inline.} =
   when not defined(danger):
     if i >= ms.len:
-      raise newException(BadIndex, formatErrorIndexBound(i, ms.len))
+      raise newException(BadIndex(), formatErrorIndexBound(i, ms.len))
   ms.mem.toCstr[i]
 
 proc `[]=`*(ms: MSlice, i: int, c: char) {.inline.} =
   when not defined(danger):
     if i >= ms.len:
-      raise newException(BadIndex, formatErrorIndexBound(i, ms.len))
+      raise newException(BadIndex(), formatErrorIndexBound(i, ms.len))
   cast[ptr char](ms.mem +! i)[] = c
 
 proc `[]`*[T, U: Ordinal](s: MSlice, x: HSlice[T, U]): MSlice {.inline.} =
