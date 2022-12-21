45d6aa63b50d3869f99e0e3e766c06a04c1904a9
Charles Blake <charlechaud@gmail.com>
2022-12-21 09:43:46 -0500

    Make hint:LineTooLong-safe.

--- test/ManualMulti.nim
+++ test/ManualMulti.nim
@@ -35,11 +35,13 @@ when isMainModule:
       # Don't have multiple Usage: stuff in there.  Also indent subcmd help.
       let use = "ManualMulti $command $args\n$doc\nOptions:\n$options"
       try:
-        discard dispatchdemo(cmdline = @[ "--help" ],prefix="    ",usage=use,noHdr=true)
+        discard dispatchdemo(cmdline = @[ "--help" ],prefix="    ",usage=use,
+                             noHdr=true)
       except HelpOnly: discard
       echo ""
       try:
-        discard dispatchshow(cmdline = @[ "--help" ],prefix="    ",usage=use,noHdr=true)
+        discard dispatchshow(cmdline = @[ "--help" ],prefix="    ",usage=use,
+                             noHdr=true)
       except HelpOnly: discard
       quit(0)
   else: echo "unknown subcommand: ", subcmd
--- test/StroppedParams.nim
+++ test/StroppedParams.nim
@@ -5,5 +5,5 @@ import cligen
 
 dispatch(repro, help = {
          "secondsSpread": "length of time window covered by one file",
-         "0": "NUL-delimit the file in --outputFile instead of newline-delimiting",
+         "0": "NUL-delimit --outputFile instead of newline-delimiting",
          "b": "single letter parameters don't suffer" })
