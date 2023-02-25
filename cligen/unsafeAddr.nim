7f1f64504d06316e24d5d8bf02813d780c3adc54
Charles Blake <charlechaud@gmail.com>
2023-02-25 15:50:26 -0500

    Add '*' export marker to finally activate this; Also add {.used.} (once it existed) to avoid unused module warnings.

--- cligen/unsafeAddr.nim
+++ cligen/unsafeAddr.nim
@@ -1,5 +1,7 @@
 ## This is a portability shim include file for when you want code to work
 ## without deprecation warnings under younger & older Nim compilers.  Just
 ## `include cligen/unsafeAdr` at the global scope before using unsafeAddr.
-when (NimMajor,NimMinor) >= (1,9):
-  template unsafeAddr(x): untyped = x.addr
+when (NimMajor,NimMinor,NimPatch) > (1,0,0):
+  {.used.}
+when (NimMajor,NimMinor) >= (1,9) and not declared(unsafeAddr):
+  template unsafeAddr*(x): untyped = x.addr
