3dcf1499243a07cb715b52693d133d14974aff44
Charles Blake <charlechaud@gmail.com>
2023-05-12 05:48:27 -0400

    Adapt to  https://github.com/nim-lang/Nim/commit/3a08e2e6ace20f086ba24360c7139852a75b93b2

--- cligen/magic.nim
+++ cligen/magic.nim
@@ -22,7 +22,7 @@ const
   cligenMagic {.booldefine.} = true
   haveMagic* = so.len > 0 and cligenMagic
 
-{.push hint[LineTooLong]:off.}
+#{.push hint[LineTooLong]:off.}
 when haveMagic:
   {. passl: so .}
   const
@@ -94,4 +94,4 @@ when haveMagic:
   const MAGIC_PARAM_BYTES_MAX* = 6
   proc magic_setparam*(m: magic_t; param: cint; val: pointer): cint {.importc.}
   proc magic_getparam*(m: magic_t; param: cint; val: pointer): cint {.importc.}
-{.pop.}
+#{.pop.}
