07b7a8d4fa8a2b8ed569e3d30cac5eb95b3c60cf
Charles Blake <charlechaud@gmail.com>
2023-07-17 07:13:49 -0400

    Make test.sh warning settings more visually similar to GNUmakefile & allow `v` to be overridden (so `v=--verbosity:1 ./test.sh` can silence the C compiler lines on very old Nim's).

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 exec < /dev/null
 export COLUMNS="80" CLIGEN_WIDTH="80" CLIGEN=/dev/null
 rm -rf $HOME/.cache/nim/*
-v="--verbosity:2"
-h="--hint[Path]:off --hint[Conf]:off --hint[Processing]:off --hint[CC]:off"
-h="$h --hint[Exec]:off --hint[Source]:off --hint[Link]:off --hint[SuccessX]:off"
-h="$h --hint[GCStats]:off"
-: ${w="--warning[Deprecated]:off --warning[ProveField]:off"}
+: ${v:="--verbosity:2"}
+h="--hint:Path:off --hint:Conf:off --hint:Processing:off --hint:CC:off"
+h="$h --hint:Exec:off --hint:Source:off --hint:Link:off --hint:SuccessX:off"
+h="$h --hint:GCStats:off --colors:off"
+: ${w="--warning:Deprecated:off --warning:ProveField:off"}
 for n in test/[A-Z]*.nim; do
   o=${n%.nim}.out
   c=$HOME/.cache/nim/cache-${n%.nim}
