c-blake/cligen

commit 3c0926c3040784b9224dc0f85266de071b548d4b

A better resolution to https://github.com/c-blake/cligen/issues/219 and also revert mistaken 929dd5d93a65463ea903a4ca51d04221a275baa1

cligen.nim
@@ -548,10 +548,10 @@ macro dispatchGen*(pro: typed{nkSym}, cmdName: string="", doc: string="",
          `cf`.helpAttrOff.getOrDefault(tag, ""))
 
       let use = if `noHdrId`:
-                  if `cf`.use.len > 0: `cf`.use  else: `usageId`
+                  if `usageId`.len > 0: `usageId` else: `cf`.use
                 else:
                   (if `cf`.useHdr.len > 0: `cf`.useHdr else: clUseHdr) &
-                    (if `cf`.use.len > 0: `cf`.use else: `usageId`)
+                    (if `usageId`.len > 0: `usageId` else: `cf`.use)
       let argStart = "[" & (if `mandatory`.len>0: `apId`.val4req&"," else: "") &
                      "optional-params]"
       `apId`.help = use % ["doc",     hl("doc", indentDoc),

commit d5f9da3d1e3ff97be1d5d5cf6aa5c8b43675419f

3c0926c3040784b9224dc0f85266de071b548d4b broke CLuser [templates]use=.. by accident resolving https://github.com/c-blake/cligen/issues/219 .
Since `usage` params default to `clUse` we cannot use .len==0 to see if
it was passed by dispatchMulti or by CLauthor/caller.  Instead we have
`dispatchMultiDG` prefix `usage` template w/"CG-TL" & look for (& trim)
that prefix inside the generated single dispatcher to change to the new
CLauthor-set top-level help only in that circumstance.

This lets every use case (except an implausible 'I want usage to start
with "CG-TL"') do what it wants.. CLusers can set `[templates]use` (eg.
to drop "Options:") for "leaf subcommands" yet still auto-drop repeated
"Usage:" in `./test/FullyAutoMulti help`-like full help dumps & still
get good top-level dumps both by default and with CLauthor amendment
for `./FullyAutoMulti -h` (as well as with no param at all, of course).

cligen.nim
@@ -604,9 +604,10 @@ macro dispatchGen*(pro: typed{nkSym}, cmdName: string="", doc: string="",
                     else: "[" & hl("clOptKeys", `posNm`) & ": " &
                           hl("clValType", `posTy`) & "...]"
       let useHdr = if `cf`.useHdr.len > 0: `cf`.useHdr else: clUseHdr
-      var use = if `usageId`.len > 0: `usageId` else: `cf`.use
-      if not `noHdrId` and "$usehdr" notin use and clUseHdr notin use:
-        use = useHdr & use
+      var use = if `usageId`.startsWith("CG-TL") and `noHdrId`: `usageId`[5..^1]
+                elif `cf`.use.len > 0: `cf`.use else: `usageId`
+      if not `noHdrId` and not("$usehdr" in use or "${usehdr}" in use) and
+         clUseHdr notin use: use = useHdr & use
       let argStart = "[" & (if `mandatory`.len>0: `apId`.val4req&"," else: "") &
                      "optional-params]"
       `apId`.help = use % ["doc",     hl("doc", indentDoc),
@@ -1134,7 +1135,7 @@ macro dispatchMultiDG*(procBkts: varargs[untyped]): untyped =
     result[^1].add(newParam("suppress", quote do: @[ "usage", "prefix" ]))
   let subDocsId = ident(prefix & "SubDocs")
   result[^1].add(newParam("usage", quote do:
-    topLevelHelp(`doc`, `use`, `cmd`, `subCmdsId`, `subDocsId`)))
+    topLevelHelp(`doc`, "CG-TL" & `use`, `cmd`, `subCmdsId`, `subDocsId`)))
 
 macro dispatchMulti*(procBrackets: varargs[untyped]): untyped =
   ## A wrapper to generate a multi-command dispatcher, call it, and quit.  The