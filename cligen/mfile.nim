167a05da0611ad9c33a8f268a5c40b4885bf47b7
Charles Blake <charlechaud@gmail.com>
2025-09-30 12:24:42 -0400

    No reason to be so terse about `fopen` failure; include `path`.

--- cligen/mfile.nim
+++ cligen/mfile.nim
@@ -298,7 +298,7 @@ iterator mSlices*(path: string, sep='\n', eat='\r', keep=false,
         else:
           yield MSlice(mem: cs, len: n)
       if f!=stdin: f.close() # stdin.close frees fd=0;Could be re-opened&confuse
-    except IOError: perror "fopen", err
+    except IOError: perror "fopen(\"" & path & "\")", err
 
 proc nSplit*(n: int, path: string, sep='\n', prot=PROT_READ, flags=MAP_SHARED):
        tuple[mf: MFile; parts: seq[MSlice]] =
