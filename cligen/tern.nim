28acaea897ce846918c29eb0f41a69cf966bd452
Charles Blake <charlechaud@gmail.com>
2025-12-31 08:33:28 -0500

    Use now ancient `sysUt.toItr` we can make `leaves` iterator much nicer like the other recursions in this file at modest expense (around 2-3%, I think).
    Also add explanation of `.cnt` field in nodes and promote indexing note
    to a doc comment.
    
    Also ditch no longer needed tree `.depth` field and tracking (which
    deletions would always have invalidated/made expensive anyway) and
    remove old broken `echoKeys` commented out impl & Nim iterator comment.
    
    Update all the `leaves` call sites to use `toItr()` for-loop macro and
    not use the as-of-this-patch retired `depth` parameter.
    
    Tweak a doc string or two along the way.

--- cligen/tern.nim
+++ cligen/tern.nim
@@ -13,16 +13,15 @@ const NUL* = '\0'
 type
   NodeOb*[T] {.acyclic.} = object
     ch*: char
-    cnt*: int
+    cnt*: int         ## Count of keys covered by this node for ``uniquePfxPat``
     when T isnot void:
       val*: T
-    kid*: array[3, ref NodeOb[T]]   #0,1,2 ~ <,=,>
+    kid*: array[3, ref NodeOb[T]]   ## 0,1,2 ~ <,=,>
   Node*[T] = ref NodeOb[T]
   Tern*[T] = object   ## A Tern can be used as either a mapping from strings
                       ## to type ``T`` or as a set(strings) if ``T`` is void.
     root*: Node[T]
     count: int        ## Number of elements
-    depth: int        ## Depth of Tree
 
 proc len*[T](t: Tern[T]): int {.inline.} = t.count
 
@@ -52,7 +51,6 @@ proc rawPfx*[T](t: Tern[T], pfx: string, longest=false): Node[T] =
   if p != nil: p.kid[1] else: p
 
 proc rawInsert*[T](t: var Tern[T], key: string): Node[T] =
-  var depth = 1
   var i = 0
   var p = t.root.addr
   while p[] != nil:
@@ -62,60 +60,33 @@ proc rawInsert*[T](t: var Tern[T], key: string): Node[T] =
       return p[]
     if d == 0: p.cnt.inc
     p = p.kid[d+1].addr
-    depth.inc
   while true:
     var n: Node[T]; new n
     n.ch = if i < key.len: key[i] else: NUL
     p[] = n
     if i.postInc == key.len:
       p.cnt = 1
-      t.depth = max(t.depth, depth)
       t.count.inc
       return n
     p.cnt.inc
     p = p.kid[1].addr
-    depth.inc
 #XXX Implement excl/delete/remove someday
 
-#[from strutils import nil
+#[
 proc print*[T](p: Node[T], depth=0) = #2,1,0 gives std emoji-orientation
   if p == nil: return                 #..i.e. ":-)" head-tilt not "(-:".
   print(p.kid[2], depth + 1)
-  echo strutils.repeat("  ", depth),cast[int](p)," ch: '",p.ch,"' cnt: ",p.cnt
+  echo repeat("  ", depth),cast[int](p)," ch: '",p.ch,"' cnt: ",p.cnt
   print(p.kid[1], depth + 1)
-  print(p.kid[0], depth + 1)
-proc echoKeys*[T](p: Node[T], pfx="", key="") =
-  if p == nil: return
-  ecKeys(p.kid[0], pfx, key)
-  if p.ch == NUL: echo pfx, key
-  else: ecKeys(p.kid[1], pfx, key & $p.ch)
-  ecKeys(p.kid[2], pfx, key) ]#
-iterator leaves[T](r: Node[T], depth:int, pfx=""): tuple[k:string, n:Node[T]] =
-  type                                               #Nim iterators should grow
-    Which = enum st0, st1, st2, stR                  #..recursion capability so
-    State = tuple[st: Which, k: string, n: Node[T]]  #..this headache can be as
-  if r != nil:                                       #..easy as echoKeys above.
-    var stack = newSeqOfCap[State](depth)
-    stack.add (st: st0, k: pfx, n: r)
-    while stack.len > 0:
-      let state = stack[^1].addr
-      if state.n == nil: break
-      case state.st
-      of st0:
-        state.st = st1
-        if state.n.kid[0] != nil:
-          stack.add (st: st0, k: state.k, n: state.n.kid[0])
-      of st1:
-        state.st = st2
-        if state.n.ch == NUL:
-          yield (k: state.k, n: state.n)
-        elif state.n.kid[1] != nil:
-          stack.add (st: st0, k: state.k & $state.n.ch, n: state.n.kid[1])
-      of st2:
-        state.st = stR
-        if state.n.kid[2] != nil:
-          stack.add (st: st0, k: state.k, n: state.n.kid[2])
-      of stR: discard stack.pop
+  print(p.kid[0], depth + 1) ]#
+proc leaves[T](r:Node[T], key=""): iterator(): tuple[k: string, n: Node[T]] =
+  iterator(): tuple[k: string, n: Node[T]] =
+    if r != nil:
+      for e in toItr(leaves(r.kid[0], key)): yield e
+      if r.ch == NUL: yield (k: key, n: r)
+      else:
+        for e in toItr(leaves(r.kid[1], key & r.ch)): yield e
+      for e in toItr(leaves(r.kid[2], key)): yield e
 
 # From here until `$` is really the same for many kinds of search tree.
 proc contains*[T](t: Tern[T], key: string): bool {.inline.} =
@@ -162,9 +133,8 @@ proc inc*[T](t: var Tern[T], key: string, val: int = 1) =
   var n = rawInsert(t, key)
   inc n.val, val
 
-proc incl*(t: var Tern[void], key: string) =
+proc incl*(t: var Tern[void], key: string) = discard rawInsert(t, key)
   ## Includes ``key`` in ``t``.
-  discard rawInsert(t, key)
 
 proc toTern*(x: openArray[string]): Tern[void] =
   ## Compute & return a ``Trie[void]`` based on input parameter.
@@ -182,29 +152,29 @@ proc `[]=`*[T](t: var Tern[T], key: string, val: T) =
 
 iterator keys*[T](t: Tern[T]): string =
   ## yields all keys in lexicographical order.
-  for k, x in t.root.leaves(t.depth): yield k
+  for k, x in toItr(t.root.leaves): yield k
 
 iterator values*[T](t: Tern[T]): T =
   ## yields all values of `t` in the lexicographical order of the
   ## corresponding keys.
-  for k, x in t.root.leaves(t.depth): yield x.val
+  for k, x in toItr(t.root.leaves): yield x.val
 
 iterator mvalues*[T](t: var Tern[T]): var T =
   ## yields all values of `t` in the lexicographical order of the
   ## corresponding keys. The values can be modified.
-  for k, x in t.root.leaves(t.depth): yield x.val
+  for k, x in toItr(t.root.leaves): yield x.val
 
 iterator items*[T](t: Tern[T]): string =
   ## yields all keys in lexicographical order.
-  for k, x in t.root.leaves(t.depth): yield k
+  for k, x in toItr(t.root.leaves): yield k
 
 iterator pairs*[T](t: Tern[T]): tuple[key: string, val: T] =
   ## yields all (key, value)-pairs of `t`.
-  for k, x in t.root.leaves(t.depth): yield (k, x.val)
+  for k, x in toItr(t.root.leaves): yield (k, x.val)
 
 iterator mpairs*[T](t: var Tern[T]): tuple[key: string, val: var T] =
   ## yields all (key, value)-pairs of `t`. The yielded values can be modified.
-  for k, x in t.root.leaves(t.depth): yield (k, x.val)
+  for k, x in toItr(t.root.leaves): yield (k, x.val)
 
 proc `$`*[T](t: Tern[T]): string =
   ## Return string form of ``t``; ``{A,B,..}`` if ``T`` is ``void`` or else
@@ -233,33 +203,28 @@ proc `$`*[T](t: Tern[T]): string =
 
 iterator keysWithPrefix*[T](t: Tern[T], prefix: string, longest=false): string =
   ## Yields all keys starting with `prefix`.
-  for k, x in rawPfx(t, prefix, longest).leaves(t.depth, prefix):
-    yield k
+  for k, x in toItr(rawPfx(t, prefix, longest).leaves(prefix)): yield k
 
 iterator valuesWithPrefix*[T](t: Tern[T], prefix: string, longest=false): T =
   ## Yields all values of ``t`` for keys starting with ``prefix``.
-  for k, x in rawPfx(t, prefix, longest).leaves(t.depth, prefix):
-    yield x.val
+  for k, x in toItr(rawPfx(t, prefix, longest).leaves(prefix)): yield x.val
 
 iterator mvaluesWithPrefix*[T](t: var Tern[T], prefix: string,
                                longest=false): var T =
   ## Yields all values of ``t`` for keys starting with ``prefix``.  The values
   ## can be modified.
-  for k, x in rawPfx(t, prefix, longest).leaves(t.depth, prefix):
-    yield x.val
+  for k, x in toItr(rawPfx(t, prefix, longest).leaves(prefix)): yield x.val
 
 iterator pairsWithPrefix*[T](t: Tern[T], prefix: string,
                              longest=false): tuple[key: string, val: T] =
   ## Yields all (key, value)-pairs of `t` starting with `prefix`.
-  for k, x in rawPfx(t, prefix, longest).leaves(t.depth, prefix):
-    yield (k, x.val)
+  for k, x in toItr(rawPfx(t, prefix, longest).leaves(prefix)): yield (k, x.val)
 
 iterator mpairsWithPrefix*[T](t: var Tern[T], prefix: string,
                               longest=false): tuple[key: string, val: var T] =
   ## Yields all (key, value)-pairs of `t` starting with `prefix`.
   ## The yielded values can be modified.
-  for k, x in rawPfx(t, prefix, longest).leaves(t.depth, prefix):
-    yield (k, x.val)
+  for k, x in toItr(rawPfx(t, prefix, longest).leaves(prefix)): yield (k, x.val)
 
 proc uniquePfxPat*[T](t: Tern[T], key: string, sep="*"): string =
   ## Return shortest unique prefix pattern for ``key`` known to be in ``t``.
@@ -277,11 +242,11 @@ proc uniquePfxPat*[T](t: Tern[T], key: string, sep="*"): string =
     p = p.kid[d+1]
 
 proc uniquePfxPats*(t: Tern[void], x: openArray[string], sep="*"): seq[string] =
-  ## Return shortest unique prefixes of ``x`` assuming non-"" & unique ``x[i]``.
+  ## Return shortest unique prefixes of ``x`` assuming no "" & unique ``x[i]``.
   result.setLen x.len; for i, s in x: result[i] = t.uniquePfxPat(s, sep)
 
 proc uniquePfxPats*(x: openArray[string], sep="*"): seq[string] =
-  ## Return shortest unique prefixes in ``x`` assuming non-"" & unique ``x[i]``.
+  ## Return shortest unique prefixes in ``x`` assuming no "" & unique ``x[i]``.
   x.toTern.uniquePfxPats x, sep
 
 proc uniqueSfxPats*(x: openArray[string], sep="*"): seq[string] =
